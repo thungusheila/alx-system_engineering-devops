d61aa433fb9318d0b738b183267102620d7e408d
thungusheila <111036458+thungusheila@users.noreply.github.com>
2023-04-25 17:31:08 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+
+'''Gather data from an API'''
+
 import requests
-import sys
+from sys import argv
 
 if __name__ == "__main__":
+    user_id = argv[1]
     url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    done_tasks = []
+    for task in todos:
+        if task.get("completed"):
+            done_tasks.append(task.get("title"))
     print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+              user.get("name"), len(done_tasks), len(todos)))
+    print("\n".join("\t {}".format(task) for task in done_tasks))
